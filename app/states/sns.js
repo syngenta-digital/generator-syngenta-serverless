698736025dd5b077a57c42ccc2eb5ba57d526410
Jordan <jordmax12@gmail.com>
2020-09-30 12:36:22 -0500

    check in

--- app/states/sns.js
+++ app/states/sns.js
@@ -1,4 +1,11 @@
 const sns = require('../../helpers/sns');
+const file = require('../../helpers/file');
+
+const timer = (time = 1000) => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    })
+}
 
 const _topic_handler = async _this => {
     return _this.prompt([
@@ -17,18 +24,27 @@ const _topic_handler = async _this => {
 
 const _subscription_handler = async _this => {
     // look at our current sqs queues and create a list of acceptable queues for user to pick?
-    return _this.prompt([
-        {
-            type    : 'input',
-            name    : 'topic_name',
-            message : `What would you like topic name to be?`
-        },
-        {
-            type    : 'input',
-            name    : 'queue_name',
-            message : `Whats the SQS queue name you are going to use for your SNS subscription trigger?`
-        }
-    ])
+    const sqs_path = `${file.root(true)}/aws/resources/sqs.yml`;
+    const sqs_exists = await file.path_exists(sqs_path);
+
+    if(sqs_exists) {
+        return _this.prompt([
+            {
+                type    : 'input',
+                name    : 'topic_name',
+                message : `Whats the ARN for the topic you are subscribing to?`
+            },
+            {
+                type    : 'input',
+                name    : 'queue_name',
+                message : `Whats the SQS queue name you are going to use for your SNS subscription trigger?`
+            }
+        ])
+    } else {
+        console.error('\n\n >>>>> ERROR: No SQS Queues in project to subscribe to! Please create a SQS resource to create an SNS Subscription resource.\n\n');
+        await timer(2000);
+        return { failed: true }
+    }
 }
 
 const _init = async _this => {
@@ -59,14 +75,15 @@ const _addSubscription = async (args) => {
 
 exports.handler = async _this => {
     const init_response = await _init(_this);
-    console.log('logging init_response', init_response)
     if(init_response.topic_or_sub.toLowerCase() === "subscription") {
         const topic_response = await _subscription_handler(_this);
-        console.log('logging topic_response', topic_response);
-        return _addSubscription(topic_response);
+        return _addSubscription(topic_response);     
     } else if (init_response.topic_or_sub.toLowerCase() === "topic") {
         const subscription_response = await _topic_handler(_this);
-        console.log('logging subscription_response', subscription_response);
-        return _addTopic(subscription_response);
+        if(!subscription_response.failed) {
+            return _addTopic(subscription_response);
+        }
+
+        return null;
     }
 }
